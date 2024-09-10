76cd78843046da1a26e31080ea271de77460c93f
AndreySozinov <savrey@ya.ru>
2024-09-10 20:44:54 +0500

    last changes

--- book.php
+++ book.php
@@ -1,45 +1,65 @@
 <?php
 
 abstract class AbstractBook {
-    private string $title;
-    private string $isbn;
-    private array $authors;
-    private int $issueYear;
-    private string $annotation;
+    protected string $title;
+    protected string $isbn;
+    protected array $authors;
+    protected int $issueYear;
+    protected string $annotation;
+    protected int $readCount = 0;
 
-    public abstract function takeBook(string $title) : AbstractBook;
+    public function __construct(string $title, string $isbn, array $authors, int $issueYear, string $annotation) {
+        $this->title = $title;
+        $this->isbn = $isbn;
+        $this->authors = $authors;
+        $this->issueYear = $issueYear;
+        $this->annotation = $annotation;
+    }
+
+    public abstract function takeBook() : string;
+
+    public function incrementReadCount() : void {
+        $this->readCount++;
+    }
+
+    public function getReadCount() : int {
+        return $this->readCount;
+    }
 }
 
-class Book extends AbstractBook{
+class PaperBook extends AbstractBook {
     private int $pagesCount;
-    private int $shelfId;
+    private string $libraryAddress;
 
-    public function takeBook(string $title) : Book {
-
+    public function __construct(string $title, string $isbn, array $authors, int $issueYear, string $annotation, int $pagesCount, string $libraryAddress) {
+        parent::__construct($title, $isbn, $authors, $issueYear, $annotation);
+        $this->pagesCount = $pagesCount;
+        $this->libraryAddress = $libraryAddress;
     }
 
-    public function returnBook(string $title) : bool {
-
-    }
-
-    public function removeBook(string $title) : bool {
-
-    }
-
-    public function addBook(string $title, string $isbn, array $authors, int $pages, int $issueYear) : bool {
-
-    }
-
-    public function placeBookInShelf(Shelf $shelf) : bool {
-
+    public function takeBook() : string {
+        $this->incrementReadCount();
+        return "You can take the book '{$this->title}' at the library located at: {$this->libraryAddress}";
     }
 }
 
 class DigitalBook extends AbstractBook {
-    private string $link;
-    private string $text;
+    private string $downloadLink;
 
-    public function takeBook(string $title) : DigitalBook {
+    public function __construct(string $title, string $isbn, array $authors, int $issueYear, string $annotation, string $downloadLink) {
+        parent::__construct($title, $isbn, $authors, $issueYear, $annotation);
+        $this->downloadLink = $downloadLink;
+    }
 
+    public function takeBook() : string {
+        $this->incrementReadCount();
+        return "You can download the digital book '{$this->title}' from the link: {$this->downloadLink}";
     }
 }
+
+
+$paperBook = new PaperBook("War and Peace", "978-3-16-148410-0", ["Lev Tolstoy"], 1869, "A classic novel.", 1225, "Library No. 5, Main St.");
+echo $paperBook->takeBook();
+
+$digitalBook = new DigitalBook("1984", "978-0-452-28423-4", ["George Orwell"], 1949, "Dystopian novel.", "https://example.com/1984");
+echo $digitalBook->takeBook();
